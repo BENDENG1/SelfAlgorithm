b75da9a26caf7f32972c262fe932d7888de1cca2
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-10-29 21:05:55 +0900

    [level 2] Title: 순위 검색, Time: 299.25 ms, Memory: 47 MB -BaekjoonHub

---
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.cpp
@@ -0,0 +1,98 @@
+/*
+순위 검색
+
+우선 그냥 복잡도 생각안하고 진행해보자
+음 바로 효율성에서 개박살 나는듯..?
+
+처음 논리 : 그냥 값들을 모두 넣고 query에 만족하는 info의 리스트를 다 탐색 하는 것이 문제임
+결국 모든 시간 타임아웃..
+
+음 그러면.. 점수에 대해서 정렬을 하고 진행?
+그래도 시간 초과
+
+맵을이용해서 푸는것은? -> 이 부분에서 음 
+
+여기서 모든 경우의 수를 string으로 만들어서 
+ex)a,b -> ab, a_, _b, __ 이렇게 4개 
+4개면 16개
+*/
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+map<string,vector<int>> m;
+
+vector<string> splitAndRemoveAnd(const string& str) {
+    vector<string> result;
+    istringstream iss(str);
+    string word;
+
+    while (iss >> word) {
+        if (word != "and") {
+            result.push_back(word);
+        }
+    }
+
+    return result;
+}
+
+int stringToInt(string s){
+    int num = 0;
+    for(char c : s){
+        num *= 10;
+        num += c - '0';
+    }
+    return num;
+}
+
+
+void splitInfo(const string& info) {
+    vector<string> infoVec = splitAndRemoveAnd(info);
+    int score = stringToInt(infoVec[4]);
+    
+    for (int i = 0; i < 16; i++) {
+        string key = "";
+        for (int j = 0; j < 4; j++) {
+            if (i & (1 << j)) {
+                key += infoVec[j];
+            } else {
+                key += "-";
+            }
+        }
+        m[key].push_back(score);
+    }
+}
+
+int findQueryFilter(const string& query) {
+    vector<string> infoVec = splitAndRemoveAnd(query);
+    string key = infoVec[0] + infoVec[1] + infoVec[2] + infoVec[3];
+    int scoreBound = stringToInt(infoVec[4]);
+
+    if (m.find(key) == m.end()) return 0;
+
+    const vector<int>& scores = m[key];
+    return scores.end() - lower_bound(scores.begin(), scores.end(), scoreBound);
+}
+
+vector<int> solution(vector<string> info, vector<string> query) {
+    vector<int> answer;
+    
+    for(string s : info){
+        splitInfo(s);
+    }
+    
+    for (auto& [key, scores] : m) {
+        sort(scores.begin(), scores.end());
+    }
+    
+    for(string s : query){
+        answer.push_back(findQueryFilter(s));
+    }
+    
+    return answer;
+}
