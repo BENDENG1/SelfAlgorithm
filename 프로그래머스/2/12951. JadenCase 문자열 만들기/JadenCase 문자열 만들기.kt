9bad34d2e3ded9b91396495bdd26ca3271ed0d3c
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-11-21 17:55:48 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.13 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.kt
@@ -1,13 +1,12 @@
 class Solution {
     fun solution(s: String): String {
         var answer = StringBuilder(s.lowercase())
-
-        for(i in answer.indices){
-            if(answer[i].isLetter()){
-                if(i == 0) 
-                    answer[i] = answer[i].uppercaseChar()
-                else if(i - 1 > 0 && answer[i - 1] == ' ') 
-                    answer[i] = answer[i].uppercaseChar()
+        
+        for(i in 0 until answer.length){
+            if(i == 0){
+                answer[i] = answer[i].uppercaseChar()
+            }else{
+                if(s[i- 1] == ' ') answer[i] = answer[i].uppercaseChar()
             }
         }
         return answer.toString()
